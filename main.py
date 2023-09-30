b1dd194294352b813562b881756bdb17a4adca9d
Sebastián Valenzuela <slvalenzu@gmail.com>
2023-09-29 21:59:24 -0300

    sin error

--- main.py
+++ main.py
@@ -32,20 +32,20 @@ conexion = create_engine(cadena_conexion)
 @app.get("/codigo/&{codigo}")
 def read_item(codigo: str):  
     while True:
-        try:
+        #try:
             sql = "SELECT Nombre, Creado, Usuario_creador,Actualizado, Actualizar_usuario FROM clients_export__2_ WHERE Código = '%s'" % codigo.upper()
             df = pd.read_sql_query(sql, con = conexion)
             return { 'State'                : 0,
                     'Descripción'           :"Consulta realizada exitosamente.",
                     'Fecha actual'          : datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-                    'Nombre'                : df['Nombre'][0],
+                    'Nombre'                : df['Nombre'][0][0],
                     'Creado'                : df['Creado'][0],
                     'Usuario creador'       : df['Usuario_creador'][0],
                     'Actualizado'           : df['Actualizado'][0],
                     'Actualizar usuario'    : df['Actualizar_usuario'][0] }
         
-        except Exception as e:
-            return {'State': 1, 'Descripción': e, 'Intento': 2}
+        #except Exception as e:
+        #    return {'State': 1, 'Descripción': e, 'Intento': 2}
 
     #    except:
      #      return {'State': 1 , 'Descripción': 'Ruta no encontrada, pruebe con otra revisando la BBDD.'}
